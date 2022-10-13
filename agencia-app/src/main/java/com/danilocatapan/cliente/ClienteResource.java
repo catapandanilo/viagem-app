448cf87b9ae4e695a7d4582eb6471b11a6af95be
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2022-10-13 11:55:44 -0300

    adicionadas rotas do crud na classe ClienteResource

--- agencia-app/src/main/java/com/danilocatapan/cliente/ClienteResource.java
+++ agencia-app/src/main/java/com/danilocatapan/cliente/ClienteResource.java
@@ -1,14 +1,48 @@
 package com.danilocatapan.cliente;
 
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import java.util.List;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/cliente")
+@Produces(MediaType.APPLICATION_JSON)
 public class ClienteResource {
 
     @GET
-    public List<Cliente> get(){
-        return Cliente.listAll();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAll(){
+        return Response.ok(Cliente.listAll()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findById(@PathParam("id") Long id){
+        return Response.ok(Cliente.findById(id)).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deleteById(@PathParam("id") Long id){
+        return Response.ok(Cliente.deleteById(id)).build();
+    }
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newCliente(Cliente cliente){
+        cliente.id = null;
+        cliente.persist();
+
+        return Response.status(Response.Status.CREATED).entity(cliente).build();
     }
 }

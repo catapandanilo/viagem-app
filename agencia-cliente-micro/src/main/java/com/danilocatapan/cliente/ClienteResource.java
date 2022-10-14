02d37157a5f3d8f50f3d3a16f315ee04153765bd
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2022-10-13 23:09:15 -0300

    replicado as classes do agencia-app

---
+++ agencia-cliente-micro/src/main/java/com/danilocatapan/cliente/ClienteResource.java
@@ -0,0 +1,47 @@
+package com.danilocatapan.cliente;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/cliente")
+public class ClienteResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAll(){
+        return Response.ok(Cliente.listAll()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente findById(@PathParam("id") Long id){
+        return Cliente.findById(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deleteById(@PathParam("id") Long id){
+        return Response.ok(Cliente.deleteById(id)).build();
+    }
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newCliente(Cliente cliente){
+        cliente.id = null;
+        cliente.persist();
+
+        return Response.status(Response.Status.CREATED).entity(cliente).build();
+    }
+}
